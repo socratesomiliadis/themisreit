18ef28e813f4f92b2f7db5291d18b1b5fb0d1733
Socrates Omiliadis <68393350+socratesomiliadis@users.noreply.github.com>
2025-08-11 04:45:20 +0400

    almost finished header changes (#1)

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,9 @@
 import { Cross as Hamburger } from "hamburger-react";
 import { useEffect, useState } from "react";
 import { gsap } from "@/lib/gsap";
-import NewsItem from "./NewsItem";
+import NewsItem from "./news-item";
 import Nav from "./Nav";
+import { cn } from "@/lib/utils";
 
 export default function Header() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -12,10 +13,19 @@ export default function Header() {
     let tl = gsap.timeline({ paused: true });
     if (isMenuOpen) {
       tl.set(".menu-body", { scale: 0.95, opacity: 0.5 }, 0);
+      tl.set(".nav-overlay", { opacity: 0 }, 0);
+      // tl.to(
+      //   "main",
+      //   {
+      //     filter: "blur(64px)",
+      //   },
+      //   0
+      // );
       tl.to(
-        "main",
+        ".nav-overlay",
         {
-          filter: "blur(64px)",
+          opacity: 1,
+          duration: 0.5,
         },
         0
       );
@@ -32,12 +42,12 @@ export default function Header() {
       tl.set(
         ".news-btn",
         {
-          x: "2rem",
-          opacity: 0.5,
+          x: "-1.5rem",
+          opacity: 0,
           // boxShadow: "0 0 0 0px #000",
           duration: 0.4,
         },
-        0.2
+        0
       );
       tl.to(
         ".news-btn",
@@ -48,56 +58,66 @@ export default function Header() {
           ease: "elastic.out(1.8,1)",
           duration: 1,
         },
-        0.2
+        0
       );
       tl.set(
         ".menu-body",
         {
           maskImage:
-            "radial-gradient(circle at calc(100% - 0rem) 1.5rem, transparent 0px, black 1px)",
+            "radial-gradient(circle at calc(100% - 0rem) 1.75rem, transparent 0px, black 1px), radial-gradient(circle at 1.75rem 1.75rem, transparent 1px, black 2px), linear-gradient(#000 0 0)",
         },
         0
       );
+      // tl.to(
+      //   ".menu-body",
+      //   {
+      //     maskImage:
+      //       "radial-gradient(circle at calc(100% - 1.75rem) 1.75rem, transparent 42px, black 43px), radial-gradient(circle at 1.75rem 1.75rem, transparent 1px, black 2px), linear-gradient(#000 0 0)",
+      //     ease: "elastic.out(1.6,1)",
+      //     duration: 1,
+      //   },
+      //   0
+      // );
       tl.to(
         ".menu-body",
         {
           maskImage:
-            "radial-gradient(circle at calc(100% - 1.5rem) 1.5rem, transparent 38px, black 39px)",
-          ease: "elastic.out(1.6,1)",
-          duration: 1,
-        },
-        0.2
-      );
-      tl.set(
-        ".nav-bg",
-        {
-          maskImage:
-            "radial-gradient(circle at 1.5rem 1.5rem, transparent 1px, black 2px)",
-        },
-        0
-      );
-      tl.to(
-        ".nav-bg",
-        {
-          maskImage:
-            "radial-gradient(circle at 1.5rem 1.5rem, transparent 38px, black 39px)",
+            "radial-gradient(circle at calc(100% - 1.75rem) 1.75rem, transparent 42px, black 43px), radial-gradient(circle at 1.75rem 1.75rem, transparent 42px, black 43px), linear-gradient(#000 0 0)",
           ease: "elastic.out(1.6,1)",
           duration: 1,
         },
         0
       );
+      // tl.set(
+      //   ".nav-bg",
+      //   {
+      //     maskImage:
+      //       "radial-gradient(circle at 1.5rem 1.5rem, transparent 1px, black 2px)",
+      //   },
+      //   0
+      // );
+      // tl.to(
+      //   ".nav-bg",
+      //   {
+      //     maskImage:
+      //       "radial-gradient(circle at 1.5rem 1.5rem, transparent 38px, black 39px)",
+      //     ease: "elastic.out(1.6,1)",
+      //     duration: 1,
+      //   },
+      //   0.15
+      // );
       tl.call(
         () => {
           setNewsOpen(true);
         },
         undefined,
-        0.2
+        0
       );
     } else {
       tl.to(
-        "main",
+        ".nav-overlay",
         {
-          filter: "blur(0px)",
+          opacity: 0,
         },
         0
       );
@@ -168,76 +188,14 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="fixed selection:bg-black selection:text-white z-[999] left-6 top-6">
-      <div className="relative flex gap-6 h-[95vh] z-20">
-        <div className="relative">
-          <div
-            style={
-              {
-                // boxShadow: "0 0 0 14px #000",
-              }
-            }
-            className="absolute left-0 top-0 z-10 rounded-full bg-white w-12 h-12 flex items-center justify-center"
-          >
-            <Hamburger
-              color="#000"
-              size={22}
-              toggled={isMenuOpen}
-              toggle={setMenuOpen}
-            />
-          </div>
-          <div
-            // style={{
-            //   boxShadow: "0 0 0 14px #000",
-            // }}
-            onClick={() => {
-              setNewsOpen((prev) => !prev);
-            }}
-            className="cursor-pointer opacity-0 absolute news-btn right-0 top-0 z-10 rounded-full bg-white text-black w-12 h-12 flex items-center justify-center"
-          >
-            <svg
-              width="50%"
-              viewBox="0 0 25 20"
-              fill="none"
-              className="transition-transform ease-out duration-200"
-              style={{
-                transform: isNewsOpen
-                  ? "rotate(0deg)"
-                  : isMenuOpen
-                  ? "rotate(180deg)"
-                  : "rotate(0deg)",
-              }}
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11.4706 19L2 10L11.4706 1M25 10L2.67647 10"
-                stroke="currentColor"
-                strokeWidth="2"
-              />
-            </svg>
-          </div>
-          <div
-            style={{
-              pointerEvents: isMenuOpen ? "auto" : "none",
-            }}
-            className="relative z-0 opacity-0 menu-body rounded-2xl h-[95vh] w-[28vw]"
-          >
-            <Nav isOpen={isMenuOpen} />
-            <div
-              style={{
-                maskImage:
-                  "radial-gradient(circle at 1.5rem 1.5rem, transparent 38px, black 39px)",
-              }}
-              className="absolute nav-bg left-0 top-0 z-0 w-full h-full bg-white"
-            ></div>
-          </div>
-        </div>
+    <header className="fixed selection:bg-black selection:text-white z-[999] right-6 top-6">
+      <div className="relative flex gap-4 h-[95vh] z-20">
         <div
           style={{
             pointerEvents: isNewsOpen ? "auto" : "none",
-            maskImage: "linear-gradient(to top, transparent, black 15%)",
+            // maskImage: "linear-gradient(to top, transparent, black 15%)",
           }}
-          className="news overflow-y-hidden h-full flex flex-col gap-4 pr-6"
+          className="news overflow-y-hidden h-full flex flex-col gap-4 pr-0"
         >
           <NewsItem
             tag="Events"
@@ -288,13 +246,74 @@ attending the B-Hype event in Dubai."
             image="/static/images/jimmy.png"
           />
         </div>
+        <div className="relative ">
+          <div
+            style={
+              {
+                // boxShadow: "0 0 0 14px #000",
+              }
+            }
+            className="absolute right-0 top-0 z-10 rounded-full bg-[#1E1E1E]/80 backdrop-blur size-14 flex items-center justify-center"
+          >
+            <Hamburger
+              color="#fff"
+              size={20}
+              toggled={isMenuOpen}
+              toggle={setMenuOpen}
+            />
+          </div>
+          <div
+            // style={{
+            //   boxShadow: "0 0 0 14px #000",
+            // }}
+            onClick={() => {
+              setNewsOpen((prev) => !prev);
+            }}
+            className="cursor-pointer opacity-0 absolute news-btn left-0 top-0 z-10 rounded-full bg-[#1E1E1E]/80 backdrop-blur text-white size-14 flex items-center justify-center"
+          >
+            <svg
+              width="50%"
+              viewBox="0 0 25 20"
+              fill="none"
+              className={cn(
+                "transition-transform ease-out duration-200",
+                isNewsOpen ? "rotate-180" : "rotate-0"
+              )}
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.4706 19L2 10L11.4706 1M25 10L2.67647 10"
+                stroke="currentColor"
+                strokeWidth="2"
+              />
+            </svg>
+          </div>
+          <div
+            style={{
+              pointerEvents: isMenuOpen ? "auto" : "none",
+              maskComposite: "exclude",
+            }}
+            className="relative z-0 opacity-0 menu-body rounded-2xl h-[95vh] w-[28vw] backdrop-blur-xl"
+          >
+            <Nav isOpen={isMenuOpen} />
+            <div
+              // style={{
+              //   maskImage:
+              //     "radial-gradient(circle at 1.5rem 1.5rem, transparent 38px, black 39px)",
+              // }}
+              className="absolute nav-bg left-0 top-0 z-0 w-full h-full bg-[#1E1E1E]/80 "
+            ></div>
+          </div>
+        </div>
       </div>
       <div
         onClick={() => {
           setMenuOpen(false);
         }}
         className="fixed left-0 top-0 z-10 w-screen h-screen"
-      ></div>
+      >
+        <div className="w-[80%] opacity-0 h-full bg-gradient-to-l from-black via-black/80 to-transparent fixed right-0 top-0 nav-overlay"></div>
+      </div>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,18 @@
-import Header from "@/components/Header";
-import { Inter } from "next/font/google";
 import Image from "next/image";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <Header />
       <main
-        className={`${inter.className} bg-[#ff6600] w-screen h-screen relative flex items-center justify-center p-24`}
+        className={` w-screen h-screen relative flex items-center justify-center p-24`}
       >
-        <div className="w-full h-full relative bg-[#080808]  rounded-2xl">
-          <Image
-            src="/static/images/BGImage.png"
-            width={1577}
-            height={2128}
-            alt=""
-            className="w-full h-full object-contain"
-          />
-        </div>
+        <Image
+          src="/static/images/flags.png"
+          alt="BGImage"
+          width={1238}
+          height={1201}
+          className="absolute top-0 right-0 w-auto h-full object-contain"
+        />
       </main>
     </>
   );
