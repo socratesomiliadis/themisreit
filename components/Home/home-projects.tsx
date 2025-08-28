c2e83f8f25a9da560aad2ab104b79e4da2284f10
socratesomiliadis <s.omiliadis@gmail.com>
2025-08-28 22:25:41 +0300

    test for project transition

--- components/Home/home-projects.tsx
+++ components/Home/home-projects.tsx
@@ -17,6 +17,7 @@ export type ProjectInfoType = {
   year: string;
   image: string;
   slug: string;
+  mainImage: string;
 };
 
 export const projectInfo: ProjectInfoType[] = [
@@ -28,6 +29,7 @@ export const projectInfo: ProjectInfoType[] = [
     year: "2025",
     image: "/static/images/logo/tacoBell.png",
     slug: "nike",
+    mainImage: "/static/images/projectHero.png",
   },
   {
     title: "Chrome Hearts",
@@ -37,6 +39,7 @@ export const projectInfo: ProjectInfoType[] = [
     year: "2025",
     image: "/static/images/logo/ch.png",
     slug: "chrome-hearts",
+    mainImage: "/static/images/projectHero.png",
   },
   {
     title: "Taco Bell",
@@ -46,6 +49,7 @@ export const projectInfo: ProjectInfoType[] = [
     year: "2025",
     image: "/static/images/logo/tacoBell.png",
     slug: "taco-bell",
+    mainImage: "/static/images/projectHero.png",
   },
   {
     title: "McDonalds",
@@ -55,6 +59,7 @@ export const projectInfo: ProjectInfoType[] = [
     year: "2025",
     image: "/static/images/logo/tacoBell.png",
     slug: "mcdonalds",
+    mainImage: "/static/images/projectHero.png",
   },
   {
     title: "Binance",
@@ -64,6 +69,7 @@ export const projectInfo: ProjectInfoType[] = [
     year: "2025",
     image: "/static/images/logo/tacoBell.png",
     slug: "binance",
+    mainImage: "/static/images/projectHero.png",
   },
   {
     title: "Microsoft",
@@ -73,6 +79,7 @@ export const projectInfo: ProjectInfoType[] = [
     year: "2025",
     image: "/static/images/logo/tacoBell.png",
     slug: "microsoft",
+    mainImage: "/static/images/projectHero.png",
   },
 ];
 
@@ -84,6 +91,7 @@ export function ProjectItem({
   year,
   slug,
   image,
+  mainImage,
 }: {
   title: string;
   type: string;
@@ -92,6 +100,7 @@ export function ProjectItem({
   year: string;
   slug: string;
   image: string;
+  mainImage: string;
 }) {
   const router = useRouter();
   const lenis = useLenis();
@@ -137,6 +146,7 @@ export function ProjectItem({
         // });
 
         const itemParent = activeItem.parentElement;
+        const itemImage = activeItem.querySelector(".project-item-image");
         const parentSibling = itemParent?.nextElementSibling;
         const parentPreviousSibling = itemParent?.previousElementSibling;
 
@@ -156,6 +166,14 @@ export function ProjectItem({
             stagger: 0.025,
           },
           0
+        );
+        tl.to(
+          itemImage,
+          {
+            opacity: 1,
+            duration: 1,
+          },
+          0.3
         );
 
         tl.to(
@@ -185,71 +203,82 @@ export function ProjectItem({
         );
       }}
       className={cn(
-        "w-full min-w-[45vw] cursor-pointer group border-y-[1px] border-white/20 flex flex-row items-center tracking-tighter relative z-10 overflow-hidden project-item",
+        "w-full min-w-[45vw] cursor-pointer group tracking-tighter relative z-10 project-item",
         `project-item-${slug}`
       )}
     >
-      <span className="w-[50%] text-white text-6xl leading-[0.75]">
-        {title}
-      </span>
-      <div
-        style={{
-          mask: "linear-gradient(to right, transparent 0%, black 15%, black 85%, transparent 100%)",
-        }}
-        className="absolute w-1/2 top-1/2  h-full overflow-hidden right-0 translate-y-[100%] group-hover:-translate-y-1/2 transition-transform duration-300 ease-out"
-      >
-        <SimpleMarquee direction="left">
-          {Array.from({ length: 10 }).map((_, index) => (
+      <div className="w-full border-y-[1px] border-white/20 flex flex-row items-center relative overflow-hidden">
+        <span className="w-[50%] text-white text-6xl leading-[0.75]">
+          {title}
+        </span>
+        <div
+          style={{
+            mask: "linear-gradient(to right, transparent 0%, black 15%, black 85%, transparent 100%)",
+          }}
+          className="absolute w-1/2 top-1/2 h-full overflow-hidden right-0 translate-y-[100%] group-hover:-translate-y-1/2 transition-transform duration-300 ease-out"
+        >
+          <SimpleMarquee direction="left">
+            {Array.from({ length: 10 }).map((_, index) => (
+              <Image
+                key={index}
+                src={image}
+                alt={title}
+                width={500}
+                height={500}
+                className="w-auto h-11"
+              />
+            ))}
+          </SimpleMarquee>
+        </div>
+        <div className="w-[50%] relative flex flex-row items-center justify-between gap-4 text-white text-sm overflow-hidden group-hover:translate-y-[100%] transition-transform duration-300 ease-out">
+          <div className="flex-col">
+            <span className="text-[#5E5E5E]">({type}) </span>
+            <span className="text-white">{subbrand}</span>
+          </div>
+          <span className="">{category}</span>
+          <div className="flex flex-row items-center gap-4">
             <Image
-              key={index}
               src={image}
               alt={title}
               width={500}
               height={500}
               className="w-auto h-11"
             />
-          ))}
-        </SimpleMarquee>
+            <Link
+              href={`/work/${slug}`}
+              className="flex flex-row items-center gap-2 justify-self-end"
+            >
+              <span className="justify-self-end">{year}</span>
+              <span className="block w-3">
+                <svg
+                  width="100%"
+                  viewBox="0 0 10 9"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M9.37205 0.379864L9.37025 7.14139L8.7499 7.14156L8.75066 4.2903L8.75142 1.43904L1.60255 8.5879L1.16401 8.14936L8.31288 1.0005L2.61036 1.00202L2.61052 0.381663L9.37205 0.379864Z"
+                    fill="white"
+                    stroke="white"
+                    stroke-width="0.5"
+                    stroke-linecap="square"
+                  />
+                </svg>
+              </span>
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="w-[50%] relative flex flex-row items-center justify-between gap-4 text-white text-sm overflow-hidden group-hover:translate-y-[100%] transition-transform duration-300 ease-out">
-        <div className="flex-col">
-          <span className="text-[#5E5E5E]">({type}) </span>
-          <span className="text-white">{subbrand}</span>
-        </div>
-        <span className="">{category}</span>
-        <div className="flex flex-row items-center gap-4">
-          <Image
-            src={image}
-            alt={title}
-            width={500}
-            height={500}
-            className="w-auto h-11"
-          />
-          <Link
-            href={`/work/${slug}`}
-            className="flex flex-row items-center gap-2 justify-self-end"
-          >
-            <span className="justify-self-end">{year}</span>
-            <span className="block w-3">
-              <svg
-                width="100%"
-                viewBox="0 0 10 9"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M9.37205 0.379864L9.37025 7.14139L8.7499 7.14156L8.75066 4.2903L8.75142 1.43904L1.60255 8.5879L1.16401 8.14936L8.31288 1.0005L2.61036 1.00202L2.61052 0.381663L9.37205 0.379864Z"
-                  fill="white"
-                  stroke="white"
-                  stroke-width="0.5"
-                  stroke-linecap="square"
-                />
-              </svg>
-            </span>
-          </Link>
-        </div>
+      <div className="absolute left-1/2 -translate-x-1/2 top-20 w-screen max-w-none px-16 project-item-image opacity-0 pointer-events-none">
+        <Image
+          src={mainImage}
+          alt={title}
+          width={1920}
+          height={1080}
+          className="w-full object-cover  "
+        />
       </div>
     </div>
   );
@@ -288,12 +317,12 @@ export default function HomeProjects() {
       )} */}
 
       <div className={cn("w-full flex flex-row gap-0 mt-8")}>
-        <div className="w-1/2 flex flex-col gap-8 overflow-hidden pr-4">
+        <div className="w-1/2 flex flex-col gap-8 pr-4">
           {firstHalfOfProjects.map((projectData: ProjectInfoType) => {
             return <ProjectItem key={projectData.slug} {...projectData} />;
           })}
         </div>
-        <div className="w-1/2 flex flex-col gap-8 overflow-hidden pl-4">
+        <div className="w-1/2 flex flex-col gap-8 pl-4">
           {secondHalfOfProjects.map((projectData: ProjectInfoType) => {
             return <ProjectItem key={projectData.slug} {...projectData} />;
           })}
--- components/ProjectSingle/project-hero.tsx
+++ components/ProjectSingle/project-hero.tsx
@@ -8,10 +8,10 @@ export default function ProjectHero({ projectData }: { projectData: any }) {
     <section className="w-screen min-h-[150vh] pt-32 px-16">
       <ProjectItem {...projectData} />
       <motion.div
-        initial={{ opacity: 0, filter: "blur(16px)" }}
-        animate={{ opacity: 1, filter: "blur(0px)" }}
-        exit={{ opacity: 0, filter: "blur(16px)" }}
-        transition={{ duration: 0.4 }}
+        // initial={{ opacity: 0, filter: "blur(16px)" }}
+        // animate={{ opacity: 1, filter: "blur(0px)" }}
+        // exit={{ opacity: 0, filter: "blur(16px)" }}
+        // transition={{ duration: 0.4 }}
         className="w-full mt-8"
       >
         <Image
