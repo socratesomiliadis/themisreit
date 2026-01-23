135f793ff3998acbc234fa0a1b4831eb1484e88e
socratesomiliadis <s.omiliadis@gmail.com>
2026-01-23 19:02:08 +0200

    added the good one

--- app/night/page.tsx
+++ app/night/page.tsx
@@ -8,26 +8,40 @@ const BakedRelief = dynamic(
   {
     ssr: false,
     loading: () => null,
-  }
+  },
 );
 
 const WEBGL_TEXTURES = {
-    bake1: "/textures/dimcha/6.png",
-    bake2: "/textures/dimcha/5.png",
-    bake3: "/textures/dimcha/4.png",
-    bake4: "/textures/dimcha/3.png",
-    bake5: "/textures/dimcha/2.png",
-    bake6: "/textures/dimcha/1.png",
-    plaster: "/textures/plaster.png",
-  };
+  bake1: "/textures/dimcha/6.png",
+  bake2: "/textures/dimcha/5.png",
+  bake3: "/textures/dimcha/4.png",
+  bake4: "/textures/dimcha/3.png",
+  bake5: "/textures/dimcha/2.png",
+  bake6: "/textures/dimcha/1.png",
+  plaster: "/textures/plaster.png",
+};
 
+const WEBGL_TEXTURES_2 = {
+  bake1: "/textures/new/0006.png",
+  bake2: "/textures/new/0005.png",
+  bake3: "/textures/new/0004.png",
+  bake4: "/textures/new/0003.png",
+  bake5: "/textures/new/0002.png",
+  bake6: "/textures/new/0001.png",
+  plaster: "/textures/plaster.png",
+};
 
 export default function NightPage() {
-  return <div className="h-screen w-screen bg-white">
-    <BakedRelief
-      className="w-full relative aspect-square h-auto"
-   
-      textures={WEBGL_TEXTURES}
-    />
-  </div>;
+  return (
+    <div className="min-h-screen w-screen">
+      <BakedRelief
+        className="w-full relative aspect-square h-auto"
+        textures={WEBGL_TEXTURES}
+      />
+      <BakedRelief
+        className="w-full relative aspect-square h-auto"
+        textures={WEBGL_TEXTURES_2}
+      />
+    </div>
+  );
 }
