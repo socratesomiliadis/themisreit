106183bc140fcd2d766e83b3b6ab534ace93ea0e
socratesomiliadis <s.omiliadis@gmail.com>
2026-02-27 15:34:37 +0200

    fix

--- apps/website/app/clients/page.tsx
+++ apps/website/app/clients/page.tsx
@@ -1,7 +1,7 @@
 import ClientsMain from "@/components/Clients/clients-main";
 import Footer from "@/components/footer";
 
-export default function ClientsPage() {
+export default function ClientswPage() {
   return (
     <>
       <main className="pb-64">
